59a91880ce114364f491ef817c2407580b99b19c
aaronx819 <50750278+aaronx819@users.noreply.github.com>
2023-10-28 11:19:48 -0400

    voice recognition

---
+++ speech.js
@@ -0,0 +1,43 @@
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new window.SpeechRecognition();
+recognition.interimResults = true;
+
+let p = document.createElement('p');
+var fileName = location.href.split("/").slice(-1); 
+recognition.addEventListener('result', (e)=> {
+	const text = Array.from(e.results)
+		.map(result => result[0])
+		.map(result => result.transcript)
+		.join('');
+	console.log(text);
+	selectPage(fileName,text);
+});
+recognition.start();
+
+function selectPage(fileName,text){
+	if(fileName == 'index.html' && text.includes("taco")){
+		location.href='selected.html';
+	}
+
+	if(fileName == 'selected.html'){
+		location.href='more.html';
+	}
+
+	if(fileName == 'more.html' && 
+		(
+			text.includes("complete") ||
+			text.includes("done")
+		)){
+			location.href='complete.html';
+	}
+
+	if(fileName == 'more.html' && 
+		(
+			text.includes("add") ||
+			text.includes("not") ||
+			text.includes("need")
+		)){
+			location.href='selected.html';
+	}
+
+}
